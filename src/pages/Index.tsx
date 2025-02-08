ea6eb861838bd3611c702e9d5cd94fa84656c0c3
BitMark Labs <97177471+BitMarkLabs@users.noreply.github.com>
2025-02-08 23:28:08 +0000

    Refactor component positioning
    Move a component to a higher position in the DOM.  The specific component and implementation details are not specified.

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -110,7 +110,7 @@ export default function Index() {
             </div>
 
             {/* New Spline Scene */}
-            <div className="w-full h-[400px] -mb-4">
+            <div className="w-full h-[400px] -mt-20">
               <Spline
                 scene="https://prod.spline.design/WPMa2X2U2NClGTaW/scene.splinecode"
                 className="w-full h-full"
