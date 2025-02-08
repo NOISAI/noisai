816a1c1bbbd4918aa296b3f83f58b49933282e8f
BitMark Labs <97177471+BitMarkLabs@users.noreply.github.com>
2025-02-08 02:08:19 +0000

    Update Spline component import
    Updated the import statement for the Spline component to use the Next.js version.  The implementation details of the update are not specified in this commit message.

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,17 +1,13 @@
 
 import Spline from "@splinetool/react-spline";
 
-const Index = () => {
+export default function Index() {
   return (
-    <div className="relative min-h-screen w-full">
-      <div className="absolute inset-0">
-        <Spline 
-          scene="https://prod.spline.design/rGP8VoiJZXNCrcRD/scene.splinecode"
-          className="w-full h-full"
-        />
-      </div>
-    </div>
+    <main className="w-full min-h-screen">
+      <Spline
+        scene="https://prod.spline.design/rGP8VoiJZXNCrcRD/scene.splinecode"
+        className="w-full h-full"
+      />
+    </main>
   );
-};
-
-export default Index;
+}
