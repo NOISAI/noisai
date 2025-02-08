2c745468f7b5bfb8f968ff971c3aa9c5749c5402
BitMark Labs <97177471+BitMarkLabs@users.noreply.github.com>
2025-02-08 02:09:38 +0000

    Investigate website display issue
    Investigate why the website is not displaying as expected.

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,7 +3,7 @@ import Spline from "@splinetool/react-spline";
 
 export default function Index() {
   return (
-    <main className="w-full min-h-screen">
+    <main className="w-screen h-screen">
       <Spline
         scene="https://prod.spline.design/rGP8VoiJZXNCrcRD/scene.splinecode"
         className="w-full h-full"
