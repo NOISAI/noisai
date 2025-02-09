9fc345d9be5bac3a44a416c3da192ac0a7cb5ea2
BitMark Labs <97177471+BitMarkLabs@users.noreply.github.com>
2025-02-09 00:22:45 +0000

    Adjust Spline scene vertical position
    Updated the vertical positioning of the Spline scene to -200px for desktop and -20px for mobile devices.

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -108,7 +108,7 @@ export default function Index() {
             </div>
 
             {/* New Spline Scene */}
-            <div className={`w-full ${isMobile ? 'h-[200px] -mt-[400px]' : 'h-[400px] -mt-[400px]'}`}>
+            <div className={`w-full ${isMobile ? 'h-[200px] -mt-[20px]' : 'h-[400px] -mt-[200px]'}`}>
               <Spline
                 scene="https://prod.spline.design/WPMa2X2U2NClGTaW/scene.splinecode"
                 className="w-full h-full"
