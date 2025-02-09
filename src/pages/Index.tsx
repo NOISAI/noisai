c92958c4bf33c55c571886b34309d3ab41c0ec9c
BitMark Labs <97177471+BitMarkLabs@users.noreply.github.com>
2025-02-09 00:30:12 +0000

    Adjust mobile breakpoint
    Update mobile breakpoint to 320px.

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -108,7 +108,7 @@ export default function Index() {
             </div>
 
             {/* New Spline Scene */}
-            <div className={`w-full ${isMobile ? 'h-[300px] mt-10' : 'h-[400px] mt-0'}`}>
+            <div className={`w-full ${isMobile ? 'h-[320px] mt-10' : 'h-[400px] mt-0'}`}>
               <Spline
                 scene="https://prod.spline.design/WPMa2X2U2NClGTaW/scene.splinecode"
                 className="w-full h-full"
